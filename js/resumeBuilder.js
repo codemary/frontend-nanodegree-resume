216eef8f53f76116572d2cac056b8c513dcf262c
Mariam Fatima <mariamfatima.co@gmail.com>
2017-03-10 20:28:22 +0530

    Added name and role to the header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Mariam Fatima");
+
+var role = "Front-End Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
